a4922d9c3c7e64e6533f74f8bc92decde87348ef
John <jkm6358@psu.edu>
2021-02-23 20:54:10 -0800

    data summary testing on 5 Year Gold Market File

--- Test_for_john.R
+++ Test_for_john.R
@@ -1,5 +1,15 @@
 #Test for John
 
+library(skimr)
+library(DataExplorer)
+
 df1 <- read.xlsx("C:/#Personal/Penn State/3. DAAN 881/Gold Market 5 Years.xlsx")
 
 summary(df1)
+
+skim(df1)
+
+#Library package DataExplorer
+#https://towardsdatascience.com/simple-fast-exploratory-data-analysis-in-r-with-dataexplorer-package-e055348d9619
+#Creates an HTML file showing data exploration summaries
+DataExplorer::create_report(df1)
